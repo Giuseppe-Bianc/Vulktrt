adf6e544555b1a76ed7c59edff184240c28df09d
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2025-03-08 13:51:14 +0100

    Enhance CMake configuration and add SIMD support
    - Updated CMake minimum version requirement to 3.23.
    - Renamed executable target from 'intro' to 'vulktrt'.
    - Introduced SIMD feature checks and support in Simd.cmake.
    - Improved compiler warning settings for MSVC, Clang, and GCC.
    - Enhanced interprocedural optimization handling and messages.
    - Added options for compiler cache and improved cache detection.
    - Updated gcovr configuration for better coverage reporting.
    - Cleaned up and organized various CMake files for better maintainability.

--- cmake/Cache.cmake
+++ cmake/Cache.cmake
@@ -1,33 +1,24 @@
 # Enable cache if available
 function(Vulktrt_enable_cache)
-  set(CACHE_OPTION
-      "ccache"
-      CACHE STRING "Compiler cache to be used")
+  set(CACHE_OPTION "ccache" CACHE STRING "Compiler cache to be used (choices: 'ccache', 'sccache')")
   set(CACHE_OPTION_VALUES "ccache" "sccache")
   set_property(CACHE CACHE_OPTION PROPERTY STRINGS ${CACHE_OPTION_VALUES})
-  list(
-    FIND
-    CACHE_OPTION_VALUES
-    ${CACHE_OPTION}
-    CACHE_OPTION_INDEX)
-
-  if(${CACHE_OPTION_INDEX} EQUAL -1)
-    message(
-      STATUS
-        "Using custom compiler cache system: '${CACHE_OPTION}', explicitly supported entries are ${CACHE_OPTION_VALUES}"
+  list(FIND CACHE_OPTION_VALUES "${CACHE_OPTION}" CACHE_OPTION_INDEX)
+  if (CACHE_OPTION_INDEX EQUAL -1)
+    message(STATUS
+            "Using custom compiler cache system: '${CACHE_OPTION}'. Supported options are: ${CACHE_OPTION_VALUES}"
     )
-  endif()
+  endif ()
 
-  find_program(CACHE_BINARY NAMES ${CACHE_OPTION_VALUES})
-  if(CACHE_BINARY)
-    message(STATUS "${CACHE_BINARY} found and enabled")
-    set(CMAKE_CXX_COMPILER_LAUNCHER
-        ${CACHE_BINARY}
-        CACHE FILEPATH "CXX compiler cache used")
-    set(CMAKE_C_COMPILER_LAUNCHER
-        ${CACHE_BINARY}
-        CACHE FILEPATH "C compiler cache used")
-  else()
-    message(WARNING "${CACHE_OPTION} is enabled but was not found. Not using it")
-  endif()
+  # Find the binary for the selected cache system (either ccache or sccache)
+  find_program(CACHE_BINARY "${CACHE_OPTION}" HINTS ENV PATH NO_CACHE)
+
+  if (CACHE_BINARY)
+    message(STATUS "Compiler cache system '${CACHE_BINARY}' found. Enabling cache.")
+    set(CMAKE_CXX_COMPILER_LAUNCHER "${CACHE_BINARY}" CACHE FILEPATH "C++ compiler cache" FORCE)
+    set(CMAKE_C_COMPILER_LAUNCHER "${CACHE_BINARY}" CACHE FILEPATH "C compiler cache" FORCE)
+  else ()
+    message(WARNING "Selected cache system '${CACHE_OPTION}' was not found. Compiler cache will not be used.")
+  endif ()
+  mark_as_advanced(CACHE_OPTION)
 endfunction()
