e032e8921bcde9a9b06fc613c609628f1ccbd6e2
Clang Robot <robot@example.com>
2025-03-08 12:55:45 +0000

    :art: Applied clang-format to source code (auto-formatting) - Updated .h, .cpp, and .hpp files to conform with the style guide. - Changes include:   - Indentation adjustments   - Spacing improvements   - Alignment enhancements   - Consistency in formatting across files   - Removal of trailing whitespace

--- src/ftxui_sample/main.cpp
+++ src/ftxui_sample/main.cpp
@@ -14,29 +14,24 @@
 // the source template at `configured_files/config.hpp.in`.
 #include <internal_use_only/config.hpp>
 
-
 // NOLINTNEXTLINE(bugprone-exception-escape)
-int main(int argc, const char **argv)
-{
-  try {
-    CLI::App app{ fmt::format("{} version {}", Vulktrt::cmake::project_name, Vulktrt::cmake::project_version) };
+int main(int argc, const char **argv) {
+    try {
+        CLI::App app{fmt::format("{} version {}", Vulktrt::cmake::project_name, Vulktrt::cmake::project_version)};
 
-    std::optional<std::string> message;
-    app.add_option("-m,--message", message, "A message to print back out");
-    bool show_version = false;
-    app.add_flag("--version", show_version, "Show version information");
+        std::optional<std::string> message;
+        app.add_option("-m,--message", message, "A message to print back out");
+        bool show_version = false;
+        app.add_flag("--version", show_version, "Show version information");
 
+        CLI11_PARSE(app, argc, argv);
 
-    CLI11_PARSE(app, argc, argv);
+        if(show_version) {
+            fmt::print("{}\n", Vulktrt::cmake::project_version);
+            return EXIT_SUCCESS;
+        }
 
-    if (show_version) {
-      fmt::print("{}\n", Vulktrt::cmake::project_version);
-      return EXIT_SUCCESS;
-    }
-
-  } catch (const std::exception &e) {
-    spdlog::error("Unhandled exception in main: {}", e.what());
-  }
+    } catch(const std::exception &e) { spdlog::error("Unhandled exception in main: {}", e.what()); }
 }
 
 // NOLINTEND(*-include-cleaner)
