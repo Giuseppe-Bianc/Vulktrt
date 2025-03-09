c9986192a9be491236ef3d1ef3cee53f40e3bcf8
Clang Robot <robot@example.com>
2025-03-09 12:20:07 +0000

    :art: Applied clang-format to source code (auto-formatting) - Updated .h, .cpp, and .hpp files to conform with the style guide. - Changes include:   - Indentation adjustments   - Spacing improvements   - Alignment enhancements   - Consistency in formatting across files   - Removal of trailing whitespace

--- include/Vulktrt/headers.hpp
+++ include/Vulktrt/headers.hpp
@@ -1,9 +1,8 @@
 /*
-* Created by gbian on 09/03/2025.
-* Copyright (c) 2025 All rights reserved.
-*/
+ * Created by gbian on 09/03/2025.
+ * Copyright (c) 2025 All rights reserved.
+ */
 
 #pragma once
-
 
 #include <vulktrt_lib_core/vulktrt_lib_core.hpp>
