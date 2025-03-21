999c5c27eb4aa2c19882aa39dbc6d247e2577ab4
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2025-03-21 12:26:05 +0100

    Refactor perspective projection calculations in Camera class for clarity and accuracy

--- src/vulktrt_lib/Camera.cpp
+++ src/vulktrt_lib/Camera.cpp
@@ -21,12 +21,13 @@ namespace lve {
     void Camera::setPerspectiveProjection(float fovy, float aspect, float near, float far) {
         assert(glm::abs(aspect - fepsilon) > 0.0f);
         const float tanHalfFovy = tan(fovy / 2.f);
+        const auto range = far - near;
         projectionMatrix = glm::mat4{0.0f};
         projectionMatrix[0][0] = 1.f / (aspect * tanHalfFovy);
         projectionMatrix[1][1] = 1.f / (tanHalfFovy);
-        projectionMatrix[2][2] = far / (far - near);
+        projectionMatrix[2][2] = far / range;
         projectionMatrix[2][3] = 1.f;
-        projectionMatrix[3][2] = -(far * near) / (far - near);
+        projectionMatrix[3][2] = -(far * near) / range;
     }
 
     void Camera::setViewDirection(const glm::vec3 &position, const glm::vec3 &direction, const glm::vec3 &up) {
